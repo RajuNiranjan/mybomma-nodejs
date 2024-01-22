c1320a6a158b82ad86f1a49c012ccfe6554580ac
RajuNiranjan <rajuniranjan1910@gmail.com>
2024-01-22 15:53:04 +0530

    created server

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
+mongoose
+  .connect(process.env.MONGO_DB)
+  .then(() => console.log("connected to mongoDB"))
+  .catch((err) => console.log(err));
 
 const app = express();
 
